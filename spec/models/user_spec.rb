161f32fc1958c5cbc4b76dedcd14a9e773e3b2c6
Kalvin Huang <kalvin_huang@yahoo.com>
2014-12-25 00:14:48 -0800

    validate presence of name and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,31 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do 
+  	@user = User.new(name: "Example User", email: "user@example.com")
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
-  it { should respond_to(:email) }
-
   #it "should respond to 'name'" do
   #	expect(@user).to respond_to(:name)
   #end
+
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+  #it "should be valid" do
+  #	expect(@user).to be_valid
+  #end
+
+  describe "when name is not present" do
+  	before { @user.name = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+  	before { @user.email = " " }
+  	it { should_not be_valid }
+  end
 end
