65a0ace229628c8314cec224da562a6405730991
Kalvin Huang <kalvin_huang@yahoo.com>
2014-12-29 01:29:00 -0800

    add correct_user before action to protect the edit/update pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
   	@user = User.new
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # Handle a successful update.
       flash[:success] = "Profile updated"
@@ -51,4 +50,9 @@ class UsersController < ApplicationController
     #  flash[:notice] = "Please sign in."
     #  redirect_to signin_url
     #end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
