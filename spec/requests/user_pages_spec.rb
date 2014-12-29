7fe6c490645998226113d19a0fc2ab5a6c2cbdfa
Kalvin Huang <kalvin_huang@yahoo.com>
2014-12-29 14:51:27 -0800

    fix user_pages_spec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,7 +6,7 @@ describe "User Pages" do
 
   describe "index" do
     let(:user) { FactoryGirl.create(:user) }
-    before do
+    before(:each) do
       sign_in user
       visit users_path
     end
@@ -27,7 +27,6 @@ describe "User Pages" do
         end
       end
     end
-  
 
     describe "delete links" do
 
@@ -101,68 +100,61 @@ describe "User Pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
-
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
+  end
 
-    describe "edit" do
-      let(:user) { FactoryGirl.create(:user) }
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
       before do
-        sign_in user
-        visit edit_user_path(user)
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
       end
 
-      describe "page" do
-        it { should have_content("Update your profile") }
-        it { should have_title("Edit user") }
-        it { should have_link('change', href: 'http://gravatar.com/emails') }
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                password_confirmation: user.password } }
       end
-
-      describe "with invalid information" do
-        before { click_button "Save changes" }
-
-        it { should have_content('error') }
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
       end
-
-      describe "with valid information" do
-        let(:new_name)  { "New Name" }
-        let(:new_email) { "new@example.com" }
-        before do
-          fill_in "Name",             with: new_name
-          fill_in "Email",            with: new_email
-          fill_in "Password",         with: user.password
-          fill_in "Confirm Password", with: user.password
-          click_button "Save changes"
-        end
-
-        it { should have_title(new_name) }
-        it { should have_selector('div.alert.alert-success') }
-        it { should have_link('Sign out', href: signout_path) }
-        specify { expect(user.reload.name).to  eq new_name }
-        specify { expect(user.reload.email).to eq new_email }
-      end
-
-      describe "forbidden attributes" do
-        let(:params) do
-          { user: { admin: true, password: user.password,
-                  password_confirmation: user.password } }
-        end
-        before do
-          sign_in user, no_capybara: true
-          patch user_path(user), params
-        end
-        specify { expect(user.reload).not_to be_admin }
-      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
+  
 end
