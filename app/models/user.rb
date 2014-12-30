797a03b88011fc2af517240607e5cebe1c062930
Kalvin Huang <kalvin_huang@yahoo.com>
2014-12-30 15:46:41 -0800

    add delete link to micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,11 @@ class User < ActiveRecord::Base
       Digest::SHA1.hexdigest(token.to_s)
 	end
 
+	def feed
+	  # This is preliminary. See "Following users" for the full implementation.
+      Micropost.where("user_id = ?", id)
+  	end
+
   	private
 
   	  # create token by getting a 16 char random string and then hashing it
